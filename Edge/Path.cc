773d434348b5827f2c8e85f6633d8aa19b7882c9
Emil Karlsson <emika583@zaza8.ida.liu.se>
2012-11-22 11:19:24 +0100

    Uppdatering av klassen Path

--- Edge/Path.cc
+++ Edge/Path.cc
@@ -1,17 +1,33 @@
 /* 
  * FILNAMN:          Path.cc
  * PROJEKT:          NEO
- * PROGRAMMERARE:    Li och Linda
+ * PROGRAMMERARE:    Li, Linda och Emil
  *
- * DATUM:            2012-11-21
+ * DATUM:            2012-11-22
  *
  * BESKRIVNING:
- * Representerar en väg mellan två noder i ett nätverk. Ärver från Set<Edge>.
+ * Representerar en väg mellan två noder i ett nätverk. Använder sig av en dubbellänkad lista för att hålla ordning på bågarna
 */
 
 #include "Path.h"
 
 using namespace std;
+
+// Defaultkonstruktor
+Path::Path()
+{
+  start_ = nullptr;
+  end_   = nullptr;
+}
+
+// Konstruktor med en specifik båge
+Path::Path(Edge* new_edge_)
+{
+  start_ = new_edge_->from;
+  end_   = new_edge_->to;
+  
+  members_.push_front(new_edge);
+}
 
 // Returnerar startnoden
 Node*
@@ -27,19 +43,34 @@ Path::end_node() const
   return end_;
 }
 
+// Returnerar första bågen
+Edge*
+Path::start_edge() const
+{
+  return members_.front();
+}
+
 // Sätter startnoden
 void
-Path::set_start_node(Node* new_start)
+Path::set_start_edge(Edge* new_start)
 {
-  start_ = new_start;
+  members_.push_front(new_start);
+  start_ = new_start->from;
 }
 
-//Sätter slutnoden
+// Sätter slutnoden
 void
-Path::set_end_node(Node* new_end)
+Path::set_end_edge(Edge* new_end)
 {
-  end_ = new_end;
+  members_.push_back(new_edge);
+  end_   = new_end->to;
 }
 
-// Tömmer mängden
-// ??????????????
+// Tömmer Path 
+void
+Path::clear()
+{
+  start_ = nullptr;
+  end_   = nullptr;
+  members_.clear();
+ }
