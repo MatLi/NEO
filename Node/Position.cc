ed349f73c7e20410460e56683cd8e3d707306b03
David Larsson <fugadux@gmail.com>
2012-11-27 18:55:40 +0100

    Städade upp Position.cc och Position.h. Konformerade till vår stilnorm.

--- Node/Position.cc
+++ Node/Position.cc
@@ -1,6 +1,8 @@
 /*
  * Fil: Position.cc
- * Programmerare: Emil Karlsson <emika583@student.liu.se>
+ * Programmerare: Emil Karlsson <emika583@student.liu.se>,
+ *                David Larsson <davla210@student.liu.se> (endast funktionen 
+ * mirror och mindre fix i rotate)
  * Datum: 2012-11-21
  *
  * En klass för representation av koordinater och vektorer i planet som används av Node.
@@ -13,7 +15,8 @@
  * Position::xpos()
  * Returnerar Positions x-koordinat.
  */
-double Position::xpos() const
+double
+Position::xpos() const
 {
   return xpos_;
 }
@@ -22,7 +25,8 @@ double Position::xpos() const
  * Position::ypos()
  * Returnerar Positions y-koordinat.
  */
-double Position::ypos() const
+double
+Position::ypos() const
 {
   return ypos_;
 }
@@ -31,7 +35,8 @@ double Position::ypos() const
  * Position::difference(Position pos)
  * Returnerar vektorn från *this till pos.
  */
-Position Position::difference(Position pos) const
+Position
+Position::difference(Position pos) const
 {
   double xpos_new = pos.xpos_ - xpos_;
   double ypos_new = pos.ypos_ - ypos_;
@@ -43,9 +48,10 @@ Position Position::difference(Position pos) const
  * Position::rotate(Position center, double angle)
  * Roterar Position angle grader runt punkten center
  */
-void Position::rotate(Position center, double angle)
+void
+Position::rotate(Position center, double angle)
 {
-  // Translaterar så att vi står i "(0,0)"
+  // Translaterar så att centrum är "(0,0)"
   this->translate(Position(- center.xpos_, - center.ypos_));
     
   // Konverterar vinkeln till radianer
@@ -54,12 +60,6 @@ void Position::rotate(Position center, double angle)
   // Uträkning
   xpos_ = xpos_*cos(angle) - ypos_*sin(angle);
   ypos_ = xpos_*sin(angle) + ypos_*cos(angle);
-  // ---
-  // Är hjälpvariablerna bara bortglömda här?
-  // Fixa eller förklara
-  // SÃ¥hÃ¤r bÃ¶r det vara. /David
-  // ---
-
   // Translaterar tillbaka till ursprungskoordinatsystemet
   this->translate(center);
 }
@@ -68,7 +68,8 @@ void Position::rotate(Position center, double angle)
  * Position::move_to(Position pos_new)
  * Flyttar Position till en ny position
  */
-void Position::move_to(Position pos_new)
+void
+Position::move_to(Position pos_new)
 {
   xpos_ = pos_new.xpos_;
   ypos_ = pos_new.ypos_;
@@ -76,41 +77,26 @@ void Position::move_to(Position pos_new)
 
 /*
  * Position::mirror(Position point, Position vect)
- * Speglar position genom linjen vect som går igenom punkten point
+ * Speglar position genom linjen med riktningsvektor vect som går genom
+ * punkten point
  */
-/*void Position::mirror(Position point, Position vect)
-{
-  //Translaterar vect och this så att vi gör point till (0,0) i vårt koordinatsystem
-  this->translate(Position(- point.xpos_, - point.ypos_));
-  vect.translate(Position(- point.xpos_, - point.ypos_));
-  // ---
-  // Ska vect.translate verkligen göras? /Jonas
-  // ---
-
-  // Uträkning av det gemensamma projektionen för x- och y-koordinaten i speglingen
-  Projection = (vect.xpos_*xpos_+vect.xpos*xpos_)/(vect.xpos_*vect.xpos_+vect.ypos_*vect.ypos_);
-  // ---
-  // vad är Projection för typ?
-  // ---
-
-  // Uträkningen för x- och y-koordinaten
-  xpos_ = 2*Projection*vect.xpos_ - xpos_;
-  ypos_ = 2*Projection*vect.ypos_ - ypos_;
-
-  // Translaterar tillbaka till ursprungskoordinatsystemet
-  this->translate(point);
-  }*/
-
 void
 Position::mirror(Position point, Position vect)
 {
+  /* Normen i kvadrat av vect. */
   double vect_norm_2 = vect.xpos()*vect.xpos() + vect.ypos()*vect.ypos();
+  /* Skalärfaktorn för projektionen på vektorn vect. */
   double scale = (vect.xpos()*xpos_ + vect.ypos()*ypos_)/vect_norm_2;
+  /* Projektionen på vektorn vect. */
   Position projection(scale*vect.xpos(),scale*vect.ypos());
 
+  /* Projektionen på normalen till vect. */
   Position normal_projection(xpos_ - projection.xpos(),
 			     ypos_ - projection.ypos());
 
+  /* Se till att vi är på rätt linje och spegla med hjälp av projektionen på
+   * normalen. Flytta sedan tillbaka till ursprungligt koordinatsystem.
+   */
   this->translate(Position(-point.xpos(),-point.ypos()));
   xpos_ = xpos_ - 2*normal_projection.xpos();
   ypos_ = ypos_ - 2*normal_projection.ypos();
@@ -123,7 +109,8 @@ Position::mirror(Position point, Position vect)
  * Position::translate(Position vect)
  * Translaterar Position med vektorn vect
  */
-void Position::translate(Position vect)
+void
+Position::translate(Position vect)
 {
   xpos_ += vect.xpos_;
   ypos_ += vect.ypos_;
