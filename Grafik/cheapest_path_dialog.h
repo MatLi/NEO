7a26630fd8643dcb0afeb0454f4737e54d8e7954
linry786 <linda.rydstrom@gmail.com>
2012-12-13 10:11:48 +0100

    cheapest_path_dialog filhuvuden och fix

--- Grafik/cheapest_path_dialog.h
+++ Grafik/cheapest_path_dialog.h
@@ -1,3 +1,14 @@
+/* 
+ * FILENAME:      cheapest_path_dialog.h
+ * PROJEKT:       NEO
+ * PROGRAMMERS:   David
+ *
+ * DATE:          2012-12-12
+ *
+ * DESCRIPTION:
+ * Dialog window that appears when solving cheapest path problems.
+ */
+
 #ifndef C_P_DIALOG_H
 #define C_P_DIALOG_H
 
